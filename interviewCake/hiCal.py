7212ebe45fd3c71f33d07786c5f36ddcd05bb448
Sandeep Solanki <manoritesandeep@gmail.com>
2024-03-22 22:06:00 -0500

    added new folder for InterviewCake files, notes and code work

---
+++ interviewCake/hiCal.py
@@ -0,0 +1,8 @@
+"""
+Your company built an in-house calendar tool called HiCal. 
+You want to add a feature to see the times in a day when everyone is available. 
+
+In HiCal, a meeting is stored as a tuple ↴ of integers (start_time, end_time). 
+These integers represent the number of 30-minute blocks past 9:00am. 
+"""
+
